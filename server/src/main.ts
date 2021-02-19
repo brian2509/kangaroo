a16932c8daaf4b5c08840cb5275c8f399359c69d
Brian <b.planje@gmail.com>
2021-02-19 23:32:08 +0100

    Add dotenv and sample .env file

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,22 +1,33 @@
 import { NestFactory } from "@nestjs/core";
 import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+import * as dotenv from "dotenv";
 import { AppModule } from "./app.module";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    cors: true,
-  });
+  const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix("api");
 
-  const config = new DocumentBuilder()
-    .setTitle("Giraffe API")
-    .setDescription("The description of the Giraffe REST API")
-    .setVersion("0.1")
-    .build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup("api", app, document);
+  // Load environment variables.
+  dotenv.config();
 
-  await app.listen(5000);
+  // Enable CORS.
+  if (process.env.NODE_ENV === "development") {
+    app.enableCors();
+  }
+
+  // Set-up Swagger code generation.
+  if (process.env.NODE_ENV === "development") {
+    const config = new DocumentBuilder()
+      .setTitle("Giraffe API")
+      .setDescription("The description of the Giraffe REST API")
+      .setVersion("0.1")
+      .build();
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup("api", app, document);
+  }
+
+  const port = process.env.PORT;
+  await app.listen(port);
 }
 
 bootstrap();
