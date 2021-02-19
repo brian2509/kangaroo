9f2e5b3ff550e64a01901b3102ebe6330e521ce6
Brian <b.planje@gmail.com>
2021-02-19 23:38:43 +0100

    Add automatic input validation on all routes

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,3 +1,4 @@
+import { ValidationPipe } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
 import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 import * as dotenv from "dotenv";
@@ -6,6 +7,12 @@ import { AppModule } from "./app.module";
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix("api");
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+    })
+  );
 
   // Load environment variables.
   dotenv.config();
