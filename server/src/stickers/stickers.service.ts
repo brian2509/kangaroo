ad3c9c573503f70f3191fcd2894fbc592c542554
Brian <b.planje@gmail.com>
2021-03-01 22:15:39 +0100

    Remove unused methods

--- server/src/stickers/stickers.service.ts
+++ server/src/stickers/stickers.service.ts
@@ -11,7 +11,6 @@ import { FilesService } from "../files/files.service";
 import { UsersService } from "../users/user.service";
 import { CreateStickerDto } from "./dto/create-sticker.dto";
 import { StickerRo } from "./dto/response-sticker.dto";
-import { UpdateStickerDto } from "./dto/update-sticker.dto";
 import { Sticker } from "./entities/sticker.entity";
 
 @Injectable()
@@ -70,38 +69,6 @@ export class StickersService {
     return (
       await this.stickerRepository.findOne({
         where: { id: savedSticker.id },
-      })
-    ).toRO();
-  }
-
-  async findAll(): Promise<StickerRo[]> {
-    const stickers = await this.stickerRepository.find();
-    return stickers.map((sticker) => {
-      return sticker.toRO();
-    });
-  }
-
-  async findOne(id: string): Promise<StickerRo> {
-    const sticker = await this.stickerRepository.findOne({
-      where: { id },
-    });
-    if (!sticker) {
-      throw new NotFoundException();
-    }
-    return sticker.toRO();
-  }
-
-  async update(
-    id: string,
-    updateStickerDto: UpdateStickerDto
-  ): Promise<StickerRo> {
-    const sticker = await this.findOne(id);
-    const updatedSticker = { ...sticker, ...updateStickerDto };
-    const saved = await this.stickerRepository.save(updatedSticker);
-
-    return (
-      await this.stickerRepository.findOne({
-        where: { id: saved.id },
       })
     ).toRO();
   }
