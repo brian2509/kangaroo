f498b568bb44b14b28b2a5c02bb45a86c868b51a
Paul van der Laan <p.j.vanderlaan-1@student.tudelft.nl>
2021-03-07 21:58:21 +0100

    Add discover account cards

--- app/src/screens/app/home/DiscoverScreen.tsx
+++ app/src/screens/app/home/DiscoverScreen.tsx
@@ -1,21 +1,59 @@
 import { StackScreenProps } from "@react-navigation/stack";
-import { Divider, Icon, Layout, List, ListItem } from "@ui-kitten/components";
+import { Button, Divider, Icon, Layout, List, ListItem, Text } from "@ui-kitten/components";
 import React, { useEffect } from "react";
 import { FeedHeader } from "../../../components/home/FeedHeader";
 import { DiscoverStackParamList } from "src/navigation/AppNavigator";
-import tailwind from "tailwind-rn";
 import { UserRo } from "src/api/generated-typescript-api-client/src";
-import { SafeAreaView } from "react-native";
+import { ScrollView } from "react-native";
 import tw from "tailwind-react-native-classnames";
 import { TextStatElement } from "../../../components/common/TextStatElement";
+import { ProfileIcon } from "../../../components/common/ProfileIcon";
+import { SafeAreaView } from "react-native";
+
+const mockUser: UserRo = {
+    id: "0",
+    username: "df",
+};
 
 type Props = StackScreenProps<DiscoverStackParamList, "DiscoverScreen">;
 
 const InfoHeader = (): React.ReactElement => {
     return (
-        <Layout style={tw`flex-row p-1 pl-5 bg-transparent`}>
+        <Layout style={tw`flex-row p-1 pl-5 bg-transparent shadow-lg`}>
             <TextStatElement value={100} text="Following"></TextStatElement>
             <TextStatElement value={1200} text="Followers"></TextStatElement>
+        </Layout>
+    );
+};
+
+const UserAccount = (): React.ReactElement => {
+    return (
+        <Layout style={tw.style(`p-2 bg-white mr-2 mb-2 flex-grow shadow-md`)}>
+            <Layout style={tw`flex-col`}>
+                <Layout style={tw`flex-row justify-between my-2`}>
+                    <Layout style={tw`border-b border-gray-300 w-9 self-center ml-3`}></Layout>
+                    <Layout style={tw`bg-transparent`}>
+                        <ProfileIcon size={16}></ProfileIcon>
+                    </Layout>
+                    <Layout style={tw`border-b border-gray-300 w-9 self-center mr-3`}></Layout>
+                </Layout>
+                <Text style={tw`text-lg font-semibold`}>Willem Alexander</Text>
+                <Text style={tw`text-xs text-gray-400`}>@willempie</Text>
+                <Layout style={tw`flex-row mt-2`}>
+                    <TextStatElement
+                        style={"text-xs"}
+                        value={100}
+                        text="Followers"></TextStatElement>
+                    {/* <TextStatElement
+                        style={"text-xs"}
+                        value={100}
+                        text="Following"></TextStatElement> */}
+                </Layout>
+                <Text style={tw`text-xs pt-1 italic text-gray-400`}>23 shared followers</Text>
+                <Button size="tiny" style={tw`text-sm p-2 mt-2 rounded-lg`}>
+                    Follow
+                </Button>
+            </Layout>
         </Layout>
     );
 };
@@ -41,10 +79,6 @@ export const DiscoverScreen = ({ navigation }: Props): React.ReactElement => {
         },
     ];
 
-    const mockUser: UserRo = {
-        id: "0",
-        username: "df",
-    };
     useEffect(() => {
         navigation.setOptions({
             headerTitle: function headerComponent() {
@@ -62,9 +96,29 @@ export const DiscoverScreen = ({ navigation }: Props): React.ReactElement => {
     });
 
     return (
-        <SafeAreaView>
+        <Layout style={tw`flex-col bg-transparent h-4/5`}>
             <InfoHeader></InfoHeader>
-            <List data={data} ItemSeparatorComponent={Divider} renderItem={renderItem} />
-        </SafeAreaView>
+            <Layout>
+                <List
+                    scrollEnabled={false}
+                    data={data}
+                    ItemSeparatorComponent={Divider}
+                    renderItem={renderItem}
+                />
+            </Layout>
+
+            <Layout style={tw`m-2 mr-0 bg-transparent`}>
+                <ScrollView>
+                    <Layout style={tw`flex-row flex-wrap bg-transparent`}>
+                        <UserAccount></UserAccount>
+                        <UserAccount></UserAccount>
+                        <UserAccount></UserAccount>
+                        <UserAccount></UserAccount>
+                        <UserAccount></UserAccount>
+                        <UserAccount></UserAccount>
+                    </Layout>
+                </ScrollView>
+            </Layout>
+        </Layout>
     );
 };
