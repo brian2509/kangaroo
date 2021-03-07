df1523edef9f5d8cac05580ec9bbd3ea00e21a02
MKuijpers <mika.kuijpers@live.nl>
2021-03-07 12:40:18 +0100

    Hook up create pack API on new screen

--- app/src/screens/app/home/createpack/CreateStickerPackScreen.tsx
+++ app/src/screens/app/home/createpack/CreateStickerPackScreen.tsx
@@ -1,41 +1,67 @@
 import { StackScreenProps } from "@react-navigation/stack";
 import { Button, CheckBox, Input, Layout, Text } from "@ui-kitten/components";
 import React, { useState } from "react";
+import { Keyboard } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { useMutation, useQueryClient } from "react-query";
 import tailwind from "tailwind-rn";
+import { CreateStickerPackDto } from "../../../../api/generated-typescript-api-client/src";
+import { api } from "../../../../api/generatedApiWrapper";
+import { QUERY_KEYS } from "../../../../constants/ReactQueryKeys";
 import { HomeStackParamList } from "../../../../navigation/AppNavigator";
+import { logErrorResponse } from "../../../../util/logging";
 
 type Props = StackScreenProps<HomeStackParamList, "CreateStickerPackScreen">;
 export const CreateStickerPackScreen = ({ navigation }: Props): React.ReactElement => {
     const [stickerPackName, setStickerPackName] = useState("");
     const [stickerPackPrivate, setStickerPackPrivate] = useState(false);
 
+    const queryClient = useQueryClient();
+
+    const createStickerPackMutation = useMutation(
+        async (createStickerPackDto: CreateStickerPackDto) =>
+            (await api.stickerPacks.create(createStickerPackDto)).data,
+        {
+            onSuccess: () => queryClient.invalidateQueries(QUERY_KEYS.myStickerPacks),
+            onError: logErrorResponse,
+        },
+    );
+
     return (
         <SafeAreaView style={tailwind("flex-1 bg-white")}>
             <Layout style={tailwind("flex-col p-8")}>
-                <Layout style={tailwind("bg-gray-100 rounded-lg mt-4 p-4")}>
-                    <Input
-                        style={tailwind("w-full py-3 rounded-lg")}
-                        size="large"
-                        label="Sticker pack name"
-                        placeholder="Name"
-                        value={stickerPackName}
-                        onChangeText={setStickerPackName}></Input>
+                <Input
+                    size="large"
+                    label="Sticker pack name"
+                    placeholder="Name"
+                    value={stickerPackName}
+                    autoFocus={true}
+                    onEndEditing={() => Keyboard.dismiss()}
+                    onChangeText={setStickerPackName}></Input>
+                <Layout style={tailwind("flex-row justify-between my-3")}>
                     <CheckBox
-                        style={tailwind("w-full py-3 px-0.5 rounded-lg")}
+                        style={tailwind("ml-3 flex-grow")}
                         checked={stickerPackPrivate}
                         onChange={setStickerPackPrivate}>
-                        {() => <Text style={tailwind("mx-3 text-base")}>Private</Text>}
+                        {() => <Text style={tailwind("mx-3 text-sm text-gray-400")}>Private</Text>}
                     </CheckBox>
+                    <Button
+                        style={tailwind("px-6")}
+                        onPress={() => {
+                            createStickerPackMutation.mutate(
+                                {
+                                    name: stickerPackName,
+                                    personal: stickerPackPrivate,
+                                    animated: false,
+                                },
+                                {
+                                    onSuccess: () => navigation.pop(),
+                                },
+                            );
+                        }}>
+                        Create!
+                    </Button>
                 </Layout>
-                <Button
-                    style={tailwind("pl-10 pr-10 my-10")}
-                    onPress={() => {
-                        console.log({ stickerPackName, stickerPackPrivate });
-                        navigation.pop();
-                    }}>
-                    Create!
-                </Button>
             </Layout>
         </SafeAreaView>
     );
