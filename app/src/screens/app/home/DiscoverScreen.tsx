e061265871cb8a7860ad7dfa0b7a07600efd5a4a
Paul van der Laan <p.j.vanderlaan-1@student.tudelft.nl>
2021-03-07 20:00:37 +0100

    Add header elements

--- app/src/screens/app/home/DiscoverScreen.tsx
+++ app/src/screens/app/home/DiscoverScreen.tsx
@@ -1,17 +1,46 @@
 import { StackScreenProps } from "@react-navigation/stack";
-import { Layout } from "@ui-kitten/components";
+import { Divider, Icon, Layout, List, ListItem } from "@ui-kitten/components";
 import React, { useEffect } from "react";
 import { FeedHeader } from "../../../components/home/FeedHeader";
 import { DiscoverStackParamList } from "src/navigation/AppNavigator";
 import tailwind from "tailwind-rn";
 import { UserRo } from "src/api/generated-typescript-api-client/src";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { ScrollView } from "react-native";
 import tw from "tailwind-react-native-classnames";
+import { TextStatElement } from "../../../components/common/TextStatElement";
 
 type Props = StackScreenProps<DiscoverStackParamList, "DiscoverScreen">;
 
+const InfoHeader = (): React.ReactElement => {
+    return (
+        <Layout style={tw`flex-row p-1 pl-5 bg-transparent`}>
+            <TextStatElement value={100} text="Following"></TextStatElement>
+            <TextStatElement value={1200} text="Followers"></TextStatElement>
+        </Layout>
+    );
+};
+
+const renderItem = ({ item }: { item: { title: string } }) => (
+    <ListItem
+        style={tw`h-10`}
+        title={`${item.title}`}
+        accessoryRight={(props) => <Icon {...props} name="arrow-ios-forward-outline" />}
+    />
+);
+
 export const DiscoverScreen = ({ navigation }: Props): React.ReactElement => {
+    const data = [
+        {
+            title: "Followers",
+        },
+        {
+            title: "Following",
+        },
+        {
+            title: "Invitations",
+        },
+    ];
+
     const mockUser: UserRo = {
         id: "0",
         username: "df",
@@ -33,8 +62,9 @@ export const DiscoverScreen = ({ navigation }: Props): React.ReactElement => {
     });
 
     return (
-        <SafeAreaView style={tailwind("w-full h-full")}>
-            <ScrollView></ScrollView>
+        <SafeAreaView style={tailwind("w-full")}>
+            <InfoHeader></InfoHeader>
+            <List data={data} ItemSeparatorComponent={Divider} renderItem={renderItem} />
         </SafeAreaView>
     );
 };
