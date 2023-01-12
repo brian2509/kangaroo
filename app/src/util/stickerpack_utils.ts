7a1dbd8f2b46e2d0ae8cf0829e37ce7df436ada9
MKuijpers <mika.kuijpers@live.nl>
2023-01-12 20:01:34 +0100

    Sort stickers of /api/sticker-packs endpoint

--- app/src/util/stickerpack_utils.ts
+++ app/src/util/stickerpack_utils.ts
@@ -1,4 +1,5 @@
-import { StickerPackRo, UserPrivateRo, UserRo } from "../api/generated-typescript-api-client/src";
+import dayjs from "dayjs";
+import { StickerPackRo, StickerRo, UserPrivateRo, UserRo } from "../api/generated-typescript-api-client/src";
 
 export const isAuthor = (stickerPack: StickerPackRo, user: UserRo | UserPrivateRo): boolean => {
     return stickerPack.author.id == user.id;
@@ -7,3 +8,7 @@ export const isAuthor = (stickerPack: StickerPackRo, user: UserRo | UserPrivateR
 export const fullMemberList = (stickerPack: StickerPackRo): UserRo[] => {
     return [stickerPack.author, ...stickerPack.members];
 };
+
+export const sortStickersByCreatedAt = (stickers: StickerRo[]): StickerRo[] => {
+    return stickers.sort((a, b) => dayjs(a.createdAt).diff(dayjs(b.createdAt)));
+}
