1ff6efcbd5e89dd0923248c01b6cdb8ead69f2c8
MKuijpers <mika.kuijpers@live.nl>
2023-01-12 20:29:50 +0100

    Add some other experimentations for sticker pack header, commented for now

--- app/src/components/home/HomeScreenHeader.tsx
+++ app/src/components/home/HomeScreenHeader.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import tailwind from "tailwind-rn";
-import { Layout, Text } from "@ui-kitten/components";
+import { Icon, Layout, Text } from "@ui-kitten/components";
 import { TouchableOpacity } from "react-native";
 import { ProfileIcon } from "../common/ProfileIcon";
 
@@ -9,13 +9,23 @@ interface Props {
 }
 export const HomeScreenHeader = ({ onPressProfile }: Props): JSX.Element => {
     return (
-        <Layout style={tailwind("flex-row p-4 pt-6 bg-gray-100 items-center")}>
-            <TouchableOpacity onPressIn={onPressProfile}>
-                <ProfileIcon size={8} />
+        <Layout style={tailwind("flex-row p-6 bg-gray-100 items-center border-b border-gray-300")}>
+            <TouchableOpacity onPressIn={onPressProfile} style={tailwind("mr-6")}>
+                <ProfileIcon size={10} />
             </TouchableOpacity>
-            <Text style={tailwind("pl-4 text-3xl font-bold")}>
+            <Text style={tailwind("text-4xl font-bold")}>
                 Sticker Packs
             </Text>
+            {/* <TouchableOpacity onPressIn={onPressProfile} style={tailwind("bg-gray-300 rounded-full p-2")}>
+                <Icon
+                    style={tailwind("w-8 h-8")}
+                    fill='#8F9BB3'
+                    name='person'
+                />
+            </TouchableOpacity> */}
+            {/* <TouchableOpacity onPressIn={onPressProfile} >
+                <ProfileIcon size={10} />
+            </TouchableOpacity> */}
         </Layout >
     );
 }
