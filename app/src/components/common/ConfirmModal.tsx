e9ed8d4849c3521d4206b1b8a682904142007c45
MKuijpers <M.Kuijpers-1@student.tudelft.nl>
2023-01-18 23:37:45 +0100

    Rewrite confirm modal as react component and use if for delete sticker confirm

--- app/src/components/common/ConfirmModal.tsx
+++ app/src/components/common/ConfirmModal.tsx
@@ -1,7 +1,7 @@
-import React from "react";
+import React, { ReactNode } from "react";
 
 import { TouchableOpacity } from "react-native";
-import { Button, Layout, ModalService, Text } from "@ui-kitten/components";
+import { Button, Layout, Modal, ModalService, Text } from "@ui-kitten/components";
 import tailwind from "tailwind-rn";
 
 interface ConfirmModalProps {
@@ -10,6 +10,28 @@ interface ConfirmModalProps {
     onPressConfirm: () => void;
     onPressCancel?: () => void;
     status?: "basic" | "primary" | "success" | "info" | "warning" | "danger" | "control";
+}
+const ConfirmModalComponent = ({ message, buttonText, onPressConfirm, onPressCancel, status = "primary" }: ConfirmModalProps) => {
+    return (
+        <TouchableOpacity
+            style={tailwind("w-full h-full flex flex-col items-center justify-center bg-black bg-opacity-50")}
+            onPress={onPressCancel}
+        >
+            <Layout style={tailwind("flex flex-col items-center bg-white py-4 rounded-2xl w-3/4")}>
+                <Text style={tailwind("my-1")}>
+                    {message}
+                </Text>
+                <Layout style={tailwind("flex flex-col justify-around mt-1")}>
+                    <Button onPress={onPressConfirm} status={status} appearance="ghost" size="large">
+                        {buttonText}
+                    </Button>
+                    <Button onPress={onPressCancel} status="basic" appearance="ghost">
+                        Cancel
+                    </Button>
+                </Layout>
+            </Layout>
+        </TouchableOpacity>
+    )
 }
 export const showConfirmModal = ({ message, buttonText, onPressConfirm, onPressCancel, status }: ConfirmModalProps): void => {
     let modalId = ''
@@ -27,26 +49,43 @@ export const showConfirmModal = ({ message, buttonText, onPressConfirm, onPressC
     };
 
     modalId = ModalService.show(
-        <TouchableOpacity
-            style={tailwind("w-full h-full flex flex-col items-center justify-center bg-black bg-opacity-50")}
-            onPress={pressCancel}
-        >
-            <Layout style={tailwind("flex flex-col items-center bg-white py-4 rounded-2xl w-3/4")}>
-                <Text style={tailwind("my-1")}>
-                    {message}
-                </Text>
-                <Layout style={tailwind("flex flex-col justify-around mt-1")}>
-                    <Button onPress={pressConfirm} status={status} appearance="ghost" size="large">
-                        {buttonText}
-                    </Button>
-                    <Button onPress={pressCancel} status="basic" appearance="ghost">
-                        Cancel
-                    </Button>
-                </Layout>
-            </Layout>
-        </TouchableOpacity>,
+        <ConfirmModalComponent
+            message={message}
+            buttonText={buttonText}
+            onPressConfirm={pressConfirm}
+            onPressCancel={pressCancel}
+            status={status}
+        />,
         {
             onBackdropPress: pressCancel,
         }
     );
+}
+
+interface ConfirmModalComponentProps extends ConfirmModalProps {
+    visible: boolean;
+    hideModal: () => void;
+}
+export const ConfirmModal = ({ visible, hideModal, message, buttonText, onPressCancel, onPressConfirm, status }: ConfirmModalComponentProps): JSX.Element => {
+    const pressConfirm = () => {
+        onPressConfirm();
+        hideModal();
+    }
+
+    const pressCancel = () => {
+        onPressCancel && onPressCancel();
+        hideModal();
+    };
+
+    return (
+        <Modal visible={visible} style={tailwind("flex-1 w-full h-full")}>
+            <ConfirmModalComponent
+                message={message}
+                buttonText={buttonText}
+                onPressConfirm={pressConfirm}
+                onPressCancel={pressCancel}
+                status={status}
+            />
+        </Modal>
+    )
 }
